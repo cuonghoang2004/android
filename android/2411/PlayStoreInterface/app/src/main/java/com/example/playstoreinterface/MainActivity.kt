648949e383ca90d3250f28bd18368530b024f931
cuonghoang2004 <cuonghoangduc867@gmail.com>
2025-11-24 23:53:15 +0700

    Add files via upload

---
+++ android/2411/PlayStoreInterface/app/src/main/java/com/example/playstoreinterface/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.playstoreinterface
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Lấy root view của activity để set padding cho hệ thống bar
+        val rootView = findViewById<android.view.View>(android.R.id.content)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
